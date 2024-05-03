a7d72d1ca6d0a01db00de13450513ac083c04f3a
Guillaume <coigniez.g@gmail.com>
2024-05-04 00:49:27 +0200

    FormField which is used inside of java

--- src/main/java/coigniez/rentapp/controllers/property/AddPropertyController.java
+++ src/main/java/coigniez/rentapp/controllers/property/AddPropertyController.java
@@ -2,10 +2,19 @@ package coigniez.rentapp.controllers.property;
 
 import org.springframework.stereotype.Component;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 import net.rgielen.fxweaver.core.FxmlView;
 
 @Component
 @FxmlView("/views/property/addProperty.fxml")
 public class AddPropertyController {
-    
+    @FXML
+    private AnchorPane formField;
+
+    @FXML
+    public void initialize() {
+        System.out.println("AddPropertyController initialized");
+        formField.getChildren().add(new javafx.scene.control.Label("Add Property Form"));
+    }
 }
