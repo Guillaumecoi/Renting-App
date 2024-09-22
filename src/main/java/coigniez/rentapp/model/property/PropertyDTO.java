bc68769ed1f831f6926b62f76e2d2d6fc3d19279
Guillaume <coigniez.g@gmail.com>
2024-09-22 13:38:10 +0200

    Propertyform class created

--- src/main/java/coigniez/rentapp/model/property/PropertyDTO.java
+++ src/main/java/coigniez/rentapp/model/property/PropertyDTO.java
@@ -1,6 +1,8 @@
 package coigniez.rentapp.model.property;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import coigniez.rentapp.model.address.AddressDTO;
 import coigniez.rentapp.model.property.tag.TagDTO;
@@ -21,4 +23,14 @@ public class PropertyDTO {
     private String name;
     private AddressDTO address;
     private Set<TagDTO> tags;
+
+    /**
+     * Set the tags of the property
+     *
+     * @param List<String> tags
+     */
+    public void setTagsFromList(List<String> tags) {
+        this.tags = tags.stream().map(TagDTO::new).collect(Collectors.toSet());
+    }
+
 }
--- src/main/java/coigniez/rentapp/view/controllers/property/PropertyFormController.java
+++ src/main/java/coigniez/rentapp/view/controllers/property/PropertyFormController.java
@@ -1,25 +1,13 @@
 package coigniez.rentapp.view.controllers.property;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
-import com.dlsc.formsfx.model.structure.Field;
-import com.dlsc.formsfx.model.structure.Form;
-import com.dlsc.formsfx.model.structure.Group;
-import com.dlsc.formsfx.model.structure.SingleSelectionField;
-import com.dlsc.formsfx.model.structure.StringField;
-import com.dlsc.formsfx.view.renderer.FormRenderer;
-
 import coigniez.rentapp.exceptions.InvalidAddressException;
-import coigniez.rentapp.model.address.AddressDTO;
 import coigniez.rentapp.model.address.Country;
 import coigniez.rentapp.model.property.PropertyDTO;
 import coigniez.rentapp.model.property.PropertyService;
@@ -48,57 +36,33 @@ public class PropertyFormController {
     @Setter
     private MainController mainController;
 
+    private PropertyForm propertyForm;
+    private TagManager tagManager;
+
     @FXML
     private VBox formField;
-
-    private Form form;
-    private FormRenderer formRenderer;
-    private List<String> selectedTags;
 
     @FXML
     public void initialize() {
         property = new PropertyDTO();
-        selectedTags = new ArrayList<>();
-        TagManager tagManager = new TagManager(propertyService.findAllTags().stream().map(TagDTO::getName).toList());
-        setForm(property);
+        tagManager = new TagManager(propertyService.findAllTags().stream().map(TagDTO::getName).toList());
+        propertyForm = new PropertyForm(property, List.of(Country.getNames()));
 
         // Add a submit button
         Button submitButton = new Button("Submit");
         submitButton.setOnAction(event -> addProperty());
 
         // Add the form and the submit button to the form field
-        formField.getChildren().add(formRenderer);
+        formField.getChildren().add(propertyForm.getFormRenderer());
         formField.getChildren().add(tagManager.getPane());
         formField.getChildren().add(submitButton);
         formField.setSpacing(10);
     }
 
-    @SuppressWarnings("rawtypes")
     private void addProperty() {
         // Get the values from the form fields
-        String name = nullIfEmpty(((StringField) form.getFields().get(0)).getValue());
-        String street = nullIfEmpty(((StringField) form.getFields().get(1)).getValue());
-        String houseNumber = nullIfEmpty(((StringField) form.getFields().get(2)).getValue());
-        String busNumber = nullIfEmpty(((StringField) form.getFields().get(3)).getValue());
-        String postalCode = nullIfEmpty(((StringField) form.getFields().get(4)).getValue());
-        String city = nullIfEmpty(((StringField) form.getFields().get(5)).getValue());
-        String province = nullIfEmpty(((StringField) form.getFields().get(6)).getValue());
-        String country = nullIfEmpty((String) (((SingleSelectionField) form.getFields().get(7)).getSelection()));
-
-        // Address is null if all fields are empty
-        AddressDTO address = Arrays.asList(street, houseNumber, busNumber, postalCode, city, province, country)
-                .stream().allMatch(Objects::isNull)
-                ? null
-                : new AddressDTO(null, street, houseNumber, busNumber, postalCode, city, province, country);
-
-        // Set the attributes of the property
-        property.setName(name);
-        property.setAddress(address);
-        property.setTags(selectedTags.stream().map(tag -> new TagDTO(tag))
-                .collect(HashSet::new, HashSet::add, HashSet::addAll));
-
-        System.out.println("The following property is submitted:" + property);
-
+        property = propertyForm.getProperty();
+        property.setTagsFromList(tagManager.getSelectedTags());
         try {
             property = propertyService.saveProperty(property);
             showSuccessAlert(property);
@@ -133,62 +97,5 @@ public class PropertyFormController {
             formField.getChildren().clear();
             initialize();
         }
-    }
-
-    private void setForm(PropertyDTO property) {
-        List<String> countries = List.of(Country.getNames());
-
-        // Set the values of the form fields
-        String name = Objects.requireNonNullElse(property.getName(), "");
-        String street = (property.getAddress() == null || property.getAddress().getStreet() == null) ? ""
-                : property.getAddress().getStreet();
-        String houseNumber = (property.getAddress() == null || property.getAddress().getHouseNumber() == null)
-                ? ""
-                : property.getAddress().getHouseNumber();
-        String busNumber = (property.getAddress() == null || property.getAddress().getBusNumber() == null) ? ""
-                : property.getAddress().getBusNumber();
-        String postalCode = (property.getAddress() == null || property.getAddress().getPostalCode() == null)
-                ? ""
-                : property.getAddress().getPostalCode();
-        String city = (property.getAddress() == null || property.getAddress().getCity() == null) ? ""
-                : property.getAddress().getCity();
-        String province = (property.getAddress() == null || property.getAddress().getProvince() == null) ? ""
-                : property.getAddress().getProvince();
-        // Get the index of the country in the list if the property has a country set
-        int index = (property.getAddress() == null) ? -1
-                : countries.indexOf(property.getAddress().getCountry());
-
-        // Create the form
-        form = Form.of(
-                Group.of(
-                        Field.ofStringType(name)
-                                .label("Name")
-                                .required("Please enter a name"),
-                        Field.ofStringType(street)
-                                .label("Street"),
-                        Field.ofStringType(houseNumber)
-                                .label("House number"),
-                        Field.ofStringType(busNumber)
-                                .label("Bus number"),
-                        Field.ofStringType(postalCode)
-                                .label("Postal code"),
-                        Field.ofStringType(city)
-                                .label("City"),
-                        Field.ofStringType(province)
-                                .label("Province"),
-                        Field.ofSingleSelectionType(countries, index)
-                                .label("Country")))
-                .title("Property Form");
-
-        // Render the form
-        formRenderer = new FormRenderer(form);
-        formRenderer.setMinWidth(800);
-    }
-
-    /**
-     * Return null if the value is empty or null
-     */
-    private String nullIfEmpty(String value) {
-        return (value == null || value.isEmpty()) ? null : value;
     }
 }
